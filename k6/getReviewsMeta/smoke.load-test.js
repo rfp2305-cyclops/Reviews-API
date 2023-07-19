a79d26470cd793ba0cf36d43c4b137cf8ff77ef8
WarrenLynes <warrenlynes@gmail.com>
2023-07-19 04:32:31 -0600

    removed test script

--- k6/getReviewsMeta/smoke.load-test.js
+++ k6/getReviewsMeta/smoke.load-test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep} from 'k6';
 
 export const options = {
-  vus: 3, // Key for Smoke test. Keep it at 2, 3, max 5 VUs
+  vus: 3, // Key for Smoke k6. Keep it at 2, 3, max 5 VUs
   duration: '1m', // This can be shorter or just a few iterations
 };
 
